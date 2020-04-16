0e76b77fb04436f8cdb1cc3ad331c8f7ab0b7292
ducd-0792 <ca.duy.du@sun-asterik.com>
2020-04-16 15:55:57 +0700

    test

--- serve.py
+++ serve.py
@@ -3,6 +3,7 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--port", help="Port for debug server to listen on", default=4000)
+parser.add_argument("--host", help="Host for debug server to listen on", default="0.0.0.0")
 parser.add_argument(
     "--profile", help="Enable flask_profiler profiling", action="store_true"
 )
